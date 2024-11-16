0df0bf56da04f6179de0e3fc5a1e96fa64d2c84f
Anirban Ghosh <2003anirbanghosh@gmail.com>
2024-11-16 19:05:54 +0530

    Updated project with recent changes

--- Delta/MAJORPROJECT/init/index.js
+++ Delta/MAJORPROJECT/init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner:"67387cd78a907ce3cf5c01d3"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
 };
--- Delta/MAJORPROJECT/models/listing.js
+++ Delta/MAJORPROJECT/models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -7,20 +8,39 @@ const listingSchema = new Schema({
         required: true,
     },
     description: String,
-    image:
-     {
-      url:{
-         type:String,
-         default:"https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-         set: (v)=> v==="" ? "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-     },
-      filename:{
-         type:String,
-      }
-     },
+    // image:
+    //  {
+    //   url:{
+    //      type:String,
+    //      default:"https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //      set: (v)=> v==="" ? "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    //  },
+    //   filename:{
+    //      type:String,
+    //   }
+    //  },
+    image: {
+        type: String,
+    }, 
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({_id : {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
